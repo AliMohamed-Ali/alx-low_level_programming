4f7e1ee059a269e1c3b285a02db453e39c094ed7
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-18 14:58:56 -0700

    .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
-/* betty style doc for function main goes there */
-/*
- *main - Entry point
- *Desc : check if rand n is negative or postive or zero 
+/**
+ * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
