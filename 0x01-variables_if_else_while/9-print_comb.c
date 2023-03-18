d72a3b8e7503461dc8c06cb64c8c5ec841a274a3
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-18 16:32:23 -0700

    .

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descreption: Number seprate be ,
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+	return (0);
+}
