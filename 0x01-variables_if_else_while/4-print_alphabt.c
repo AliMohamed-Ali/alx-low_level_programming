2fd3cefe3723e3212f48df96fedc49fd1122926b
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-18 16:00:52 -0700

    .

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Descreption: alpha
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 'a';
+	while (i <= 'z')
+	{
+		if (i != 'e' && i != 'q')
+			putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
