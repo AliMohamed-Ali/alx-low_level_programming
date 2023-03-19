51d7cd884920909c7085325ba4575cc7fc231c5a
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-18 17:21:42 -0700

    .

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+/**
+ * main - Entry point
+ *
+ * Descreption: Number seprate be ,
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit1 = 0, digit2, digit3;
+	bool check1, check2;
+	while (digit1 <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			digit3 = 0;
+			while (digit3 <= 9)
+			{
+				check1 = digit1 != digit2 && digit1 != digit3;
+				check2 = digit1 < digit2 && digit2 < digit3;
+				if (check1 && check2)
+				{
+					putchar(digit1 + 48);
+					putchar(digit2 + 48);
+					putchar(digit3 + 48);
+					if (digit1 + digit2 + digit3 != 24)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+				digit3++;
+			}
+			digit2++;
+		}
+		digit1++;
+
+	}
+	putchar('\n');
+	return (0);
+}
