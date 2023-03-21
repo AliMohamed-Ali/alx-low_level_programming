9d35c7ec376f9c06ea25867c8833555a1411602b
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-21 16:35:23 -0700

    .

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,35 +1,21 @@
 #include "main.h"
 /**
  * jack_bauer - Entry point
- * 
  * Description: print every minute
  * Return: Always reurn void
  */
 void jack_bauer(void)
 {
-	int hour;
-	int minute;
+	int hour, minute;
 
-	for (hour = 0; hour < 24; hour++ )
-	{
+	for (hour = 0; hour < 24; hour++)
 		for (minute = 0; minute < 60; minute++)
 		{
-			if (hour < 10)
-			{
-				_putchar('0');
-				_putchar(hour + '0');
-			}
-			else
-				_putchar(hour + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(58);
-			if (minute < 10)
-			{
-				_putchar('0');
-				_putchar(minute + '0');
-			}
-			else
-				_putchar(minute + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
 		}
-	}
 }
