4e80465e26e3597d59ca57b019a0e8ccd8ca4bd7
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-21 16:57:12 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,7 +19,7 @@ void times_table(void)
 			if (result <= 9)
 				_putchar(' ');
 			else
-				_putchar((esult / 10) + '0');
+				_putchar((result / 10) + '0');
 			_putchar((result % 10) + '0');
 		}
 		_putchar('\n');
