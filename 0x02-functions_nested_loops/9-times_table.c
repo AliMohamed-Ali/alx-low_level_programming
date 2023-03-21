632529bb27d2644dd9e8442f8e749d732543a102
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-21 16:55:26 -0700

    .

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ * Description: print every minute
+ * Return: Always reurn void
+ */
+void times_table(void)
+{
+	int row, col, result;
+
+	for (row = 0; row <= 9; row++)
+	{
+		_putchar(48);
+		for (col = 1; col <= 9; col++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			result = row * col;
+			if (result <= 9)
+				_putchar(' ');
+			else
+				_putchar((esult / 10) + '0');
+			_putchar((result % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
