c772aba45f1f8354df1a1b4a6bf7d071ea30b4c5
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-21 17:23:19 -0700

    .

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ * @n: The character to check
+ * Description: print from 0 to 98
+ * Return: Always reurn void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+		for (i = n; i > 98; i--)
+			printf("%d, ", i);
+	else
+		for (i = n; i < 98; i++)
+			printf("%d, ", i);
+	printf(98);
+}
