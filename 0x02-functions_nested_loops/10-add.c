7abb4fd36e42987dc84e7cfe4a36efa905b1c8b2
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-21 17:02:39 -0700

    .

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add- Entry point
+ *
+ * Description: add two number
+ *@a: The first number
+ *@b: The second number
+ * Return: Always int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
