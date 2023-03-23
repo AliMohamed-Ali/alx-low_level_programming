65c560fb450f7729aae4060890209579e18eab2f
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-23 14:49:23 -0700

    .

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * main - Entry point print from 0 to 100
+ * main - print from 0 to 100
  * but number % 3 print Fizz and % 5print Buzz and both print FizzBUzz
+ * Return: always 0(Success)
  */
 
-int main (void)
+int main(void)
 {
 	int i;
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 && i % 5 ==0)
+		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
 		else if (i % 3 == 0)
 			printf("Fizz ");
 		else if (i % 5 == 0)
 			printf("Buzz ");
-		else 
+		else
 			printf("%d ", i);
 	}
 	printf("\n");
