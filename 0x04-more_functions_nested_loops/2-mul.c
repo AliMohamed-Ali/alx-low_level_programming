330bedf907b6674ffd5737b5d2e51594df812942
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-23 13:27:52 -0700

    .

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - take a and b then return multiple a*b
+ * @a: first number int
+ * @b: second number int
+ * Return: int a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
