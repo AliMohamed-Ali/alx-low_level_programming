7760386575ef2d69c9263201b58934a6a91f8749
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-23 13:21:09 -0700

    .

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _isdigit - take n and return 0 || 1
+ * Description: check character is digit or not
+ * @c: check character
+ * Return: 0(notSuccess) or 1(Success)
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
