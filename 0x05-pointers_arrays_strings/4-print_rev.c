526a0420f433ccfce65b0d274b770e7c822a26da
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-27 16:11:56 -0700

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_rev - take pointer and print string in reverse
+ * @s: pointer of string
+ */
+void print_rev(char *s)
+{
+	int str_len = _strlen(char *s);
+	char str[] = *s;
+
+	for (i = str_len; i >= 0; i--)
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
