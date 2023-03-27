9cfd6f428577ac40ac494caaf6ef100de848dc11
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-27 15:56:29 -0700

    .

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - take pointer and print string
+ * @str: pointer of string
+ */
+void _puts(char *str)
+{
+	while (1)
+	{
+		if (*s == '\0')
+		{
+			_putchar('\n');
+			break;
+		_putchar(*s);
+		s++;
+	}
+	return (i);
+}
