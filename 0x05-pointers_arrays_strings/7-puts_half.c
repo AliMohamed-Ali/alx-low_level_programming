519b1725730d254a97688f6ad19d12d4d9d8e646
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-27 17:45:57 -0700

    .

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,15 +9,7 @@ void puts_half(char *s)
 
 	while (s[i])
 		i++;
-	if (i % 2 == 0)
-	{
-		for (j = i / 2; j <= i; j++)
-			_putchar(s[j]);
-	}
-	else
-	{
-		for (j = (i - 1) / 2; j <= i; j++)
-			_putchar(s[j]);
-	}
+	for (j = i / 2; j <= i; j++)
+		_putchar(s[j]);
 	_putchar('\n');
 }
