7702cb42da516c2244e4275cf3062939d656f44f
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-29 15:21:27 -0700

    .

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strncpy - copy src with des
+ * @src: second string
+ * @dest: first string
+ * @n: number of character in src
+ * Return: char dest pointer
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n && *src != '\0'; j++)
+		dest[j] = src[j];
+	return (dest);
+}
