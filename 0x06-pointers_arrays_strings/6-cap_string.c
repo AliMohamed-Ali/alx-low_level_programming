e36a4b1dd0ac4c22f9d8ed94fdba2c74ae7cd11d
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-03-30 15:35:37 -0700

    .

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,53 @@
+#include"main.h"
+/**
+ * is_lower - check character is lower case
+ * @c: character
+ * Return: true 1 or false 0
+ */
+int is_lower(char c)
+{
+	return (c >= 97 && c <= 122);
+}
+
+/**
+ * isDelimiter - check character is delimiter or not
+ * @c: character
+ * Return: true 1 or false 0
+ */
+int isDelimiter(char c)
+{
+	int i;
+	char del[] = " \n\t,;.!?\"{}";
+
+	for (i = 0; i < 12; i++)
+	{
+		if (c == del[i])
+			return (1);
+	}
+	return (0);
+}
+
+/**
+ * cap_string - change all lowercase to uppercase start of word
+ * @s: string of character
+ * Return: pionter to string
+ */
+char *cap_string(char *s)
+{
+	int foundDil = 1;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (isDelimiter(s[i]))
+			foundDil = 1;
+		else if (is_lower(s[i]) && foundDil)
+		{
+			s[i] -= 32;
+			foundDil = 0;
+		}
+		else
+			foundDil = 0;
+
+	}
+	return (s);
+}
