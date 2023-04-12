8ca592c048c6c5dab4c71697723e0393476c8385
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-04-12 10:18:09 -0700

    .

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the name of function
+ * @argc: int
+ * @argv: list
+ * Return: 0 sucess
+*/
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    printf("%s\n",argv[0]);
+    return 0;
+}
