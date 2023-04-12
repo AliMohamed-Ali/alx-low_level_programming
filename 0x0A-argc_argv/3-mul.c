f52bbefa10b001501c0666fd6bcba360c709aba6
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-04-12 10:49:34 -0700

    .

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the multiple argument
+ * @argc: int
+ * @argv: list
+ * Return: int
+*/
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d\n", argv[1] * argv[2]);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+
+	}
+}
