43ab72bdd7781e36f2fee913da72b0aef1b08c83
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-04-18 06:16:48 -0700

    .

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+*struct dog - a dog struct
+*@name: dog name
+*@age: dog age
+*@owner: dog owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif /* _SUM_H */
