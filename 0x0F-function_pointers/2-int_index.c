d4f74c41192d6b5a2e75b51945505ce85a74a95d
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-04-19 11:00:21 -0700

    .

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - xecutes a function given as
+ * a parameter on each element of an array.
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer of function to take action
+ * Return: int
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i = 0;
+
+if (array && size && action)
+{
+while (i < size)
+{
+if (cmp(array[i]))
+{
+return (i);
+}
+i++;
+}
+}
+return (-1);
+}
+
