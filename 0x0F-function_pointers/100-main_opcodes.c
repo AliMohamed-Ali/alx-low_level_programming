f37088782702563f266ae71328150c9998feca25
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-04-20 05:45:22 -0700

    .

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,19 +8,17 @@
  * @argv: argument vector
  * Return: Always 0.
 */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-  char *p = (char *)main;
-  int b;
-  if (argc != 2)
-    printf("Error\n"), exit(1);
-  b = atoi(argv[1]);
-  if (b < 0)
-     printf("Error\n"), exit(2);
-  while (b--)
-    printf("%02hhx%s", *p++, b ? " " : "\n");
-  
-  
-  
-  return (0);
+	char *p = (char *)main;
+	int b;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	b = atoi(argv[1]);
+	if (b < 0)
+		printf("Error\n"), exit(2);
+	while (b--)
+		printf("%02hhx%s", *p++, b ? " " : "\n");
+	return (0);
 }
