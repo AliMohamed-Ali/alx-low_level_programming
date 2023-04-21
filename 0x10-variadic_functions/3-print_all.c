6cf257deec19138a1bed0a717c73fdd8d14b0d41
AliMohamed-Ali <elsayedali3632@gmail.com>
2023-04-20 17:09:54 -0700

    .

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,48 +1,47 @@
-
 #include "variadic_functions.h"
 
 /**
  * format_char - that prints strings, followed by a new line.
- * @app: argument pointer
+ * @ap: argument pointer
  * @separator: separator betweent number
  */
-void format_char(char *separator, va_list app)
+void format_char(char *separator, va_list ap)
 {
-	printf("%s%c", separator, va_arg(app, int));
+    printf("%s%c", separator, va_arg(ap, int));
 }
 /**
  * format_int - that prints strings, followed by a new line.
- * @app: argument pointer
+ * @ap: argument pointer
  * @separator: separator betweent number
  */
-void format_int(char *separator, va_list app)
+void format_int(char *separator, va_list ap)
 {
-	printf("%s%d", separator, va_arg(app, int));
+    printf("%s%d", separator, va_arg(ap, int));
 }
 /**
  * format_float - that prints strings, followed by a new line.
- * @app: argument pointer
+ * @ap: argument pointer
  * @separator: separator betweent number
  */
-void format_float(char *separator, va_list app)
+void format_float(char *separator, va_list ap)
 {
-	printf("%s%f", separator, va_arg(app, double));
+    printf("%s%f", separator, va_arg(ap, double));
 }
 /**
  * format_string - that prints strings, followed by a new line.
- * @app: argument pointer
+ * @ap: argument pointer
  * @separator: separator betweent number
  */
-void format_string(char *separator, va_list app)
+void format_string(char *separator, va_list ap)
 {
-char *str = va_arg(app, char *);
+    char *str = va_arg(ap, char *);
 
-switch ((int)(!str))
-{
-	case 1:
-		str = "(nil)";
-}
-	printf("%s%s", separator, str);
+    switch ((int)(!str))
+    {
+        case 1:
+            str = "(nil)";
+    }
+    printf("%s%s", separator, str);
 }
 /**
  * print_all - that prints all format, followed by a new line.
@@ -50,33 +49,33 @@ switch ((int)(!str))
  */
 void print_all(const char * const format, ...)
 {
-int i = 0, j;
-char *separator = "";
-va_list app;
-token_t tokens[] = {
-{'c', format_char},
-{'i', format_int},
-{'f', format_float},
-{'s', format_string},
-{NULL, NULL}
-};
+    int i = 0, j;
+    char *separator = "";
+    va_list ap;
+    token_t tokens[] = {
+        {'c', format_char},
+        {'i', format_int},
+        {'f', format_float},
+        {'s', format_string},
+        {NULL, NULL}
+        };
 
-va_start(app, format);
-while (format && format[i])
-{
-	j = 0;
-	while (tokens[j].token)
-	{
-		if (format[i] == tokens[j].token[0])
-		{
-			tokens[j].f(separator, app);
-			separator = ", ";
-		}
-		j++;
-	}
-	i++;
+    va_start(ap, format);
+    while (format && format[i])
+    {
+        j = 0;
+        while (tokens[j].token)
+        {
+            if (format[i] == tokens[j].token[0])
+            {
+                tokens[j].f(separator, ap);
+                separator = ", ";
+            }
+            j++;
+        }
+        i++;
 
-}
-printf("\n");
-va_end(app);
+    }
+    printf("\n");
+    va_end(ap);
 }
